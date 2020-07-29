4c102de311c7135110bf286f8d5593cecd507825
wyz23 <1156489892@qq.com>
2020-07-29 21:41:40 +0800

    logger

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,13 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*打印日志*/
+/*import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public static void main(String[] args) {
+	Logger logger = LoggerFactory.getLogger(HelloController.class);
+	logger.info("Hello world");
+}*/
 @RestController
 public class HelloController {
 
@@ -10,4 +17,6 @@ public class HelloController {
 	public String hello() {
 		return "hello world quick";
 	}
+	
+	
 }
--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -8,6 +8,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.demo.bean.Person;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /*
  * RunWith(SpringRunner.class) 使用sping的运行环境
  * 类和方法必须要是public 修饰，否则运行会报initializationError错误
@@ -24,11 +27,24 @@ import com.example.demo.bean.Person;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-	@Autowired
-	Person person;
+//	@Autowired
+//	Person person;
+	Logger logger =LoggerFactory.getLogger(getClass());
 	@Test
 	public void contextLoads() {
-		System.out.println(person);
+//		System.out.println(person);
+		//日志级别由低到高，trace < debug < info < warn < error
+		//可以调整输出的日志级别，日志就只会在这个级别以后的高级别生效 ，springboot默认是info级别
+		/*在application.yml文件里配置级别，
+		logging:
+		  level:
+			    com.example.demo: trace*/
+		logger.trace("这是trace日志。。。。");
+		logger.debug("这是debug日志。。。。");
+		logger.info("这是info日志。。。。");
+		logger.warn("这是warn日志。。。。");
+		logger.error("这是error日志。。。。");
+		
 	}
 
 }
