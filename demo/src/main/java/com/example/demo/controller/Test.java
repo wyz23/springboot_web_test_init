e76979223edcafa6e5892864e888673b85925ee4
wyz23 <1156489892@qq.com>
2020-07-31 00:06:58 +0800

    BufferedReader

--- demo/src/main/java/com/example/demo/controller/Test.java
+++ demo/src/main/java/com/example/demo/controller/Test.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 public class Test {
@@ -22,23 +25,46 @@ public class Test {
 		// System.out.println(output);
 		// input.close();
 		// output.close();
-		File fileA = new File("F:" + File.separator + "infoa.txt");
-		File fileB = new File("F:" + File.separator + "infob.txt");
-		InputStream inputA = new FileInputStream(fileA);
-		InputStream inputB = new FileInputStream(fileB);
-		ByteArrayOutputStream output = new ByteArrayOutputStream();
-		int temp =0;
-		while ((temp = inputA.read()) != -1) {
-			output.write(temp);
+//		**********************************************************************
+//		File fileA = new File("F:" + File.separator + "infoa.txt");
+//		File fileB = new File("F:" + File.separator + "infob.txt");
+//		InputStream inputA = new FileInputStream(fileA);
+//		InputStream inputB = new FileInputStream(fileB);
+//		ByteArrayOutputStream output = new ByteArrayOutputStream();
+//		int temp =0;
+//		while ((temp = inputA.read()) != -1) {
+//			output.write(temp);
+//		}
+//		while ((temp = inputB.read()) != -1) {
+//			output.write(temp);
+//		}
+//		//现在所有的内容都保存在了内存输出流里面，所有的内容变为字符数组取出
+//		byte data[] = output.toByteArray();
+//		output.close();
+//		inputA.close();
+//		inputB.close();
+//		System.out.println(new String(data));
+//		**********************************************************************
+//		System.in是InputStream类对象
+//		BufferedReader的构造方法里面需要接受Reader类对象
+//		利用InputStreamReader将字节流变为字符流
+//		BufferedReader buf =new BufferedReader(new InputStreamReader(System.in));
+//		System.out.println("请输入数据：");
+//		String str = buf.readLine();  //以回车作为换行
+//		if(str.matches("\\d{1,3}") ) {
+//			System.out.println("是数字");
+//		}
+//		System.out.println("输入的内容：" + str);
+//		**********************************************************************
+		//BufferedReader 读文件速度比InputStream方便
+		//readLine(); 每次只能读取一行数据，my.txt有很多行数据
+		File file = new File("F:" + File.separator + "my.txt");
+		BufferedReader buf  = new BufferedReader(new FileReader(file));
+		String str = null;
+		//(str = buf.readLine()) != null ；  读到最后为null是才停止读
+		while ((str = buf.readLine()) != null) {
+			System.out.println(str);
 		}
-		while ((temp = inputB.read()) != -1) {
-			output.write(temp);
-		}
-		//现在所有的内容都保存在了内存输出流里面，所有的内容变为字符数组取出
-		byte data[] = output.toByteArray();
-		output.close();
-		inputA.close();
-		inputB.close();
-		System.out.println(new String(data));
+		buf.close();
 	}
 }
